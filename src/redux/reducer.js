e8eb40e308d3c019fd170172eaa028a56d6e922a
Syed Hassnain Iqrar <hassnain.iqrar@dubizzlelabs.com>
2024-06-05 19:00:33 +0500

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -14,29 +14,38 @@ const taskReducer = (state = initialData, action) => {
           { text: action.payload, completed: false },
         ],
       };
-    case DELETE_TASK:
-      return {
-        ...state,
-        taskItems: [
-          ...state.taskItems.filter((task) => task.text !== action.payload),
-        ],
-      };
-    case COMPLETE_TASK:
-      let updatedTaskItems = []
-      state.taskItems.map((task) => {
-        if(task.text === action.payload) {
-          if(task.completed === true) {
-            task.completed = false
-          }else{
-            task.completed = true
-          }
-        }
-        updatedTaskItems.push(task)
-      })
-      return {
-        ...state,
-        taskItems: updatedTaskItems
+
+
+      
+      //---------------Previous Delete Task------------------
+    // case DELETE_TASK:
+    //   return {
+    //     ...state,
+    //     taskItems: [
+    //       ...state.taskItems.filter((task) => task.text !== action.payload),
+    //     ],
+    //   };
+
+
+      //------------------Updated Deleteb Task with Slice ()-----------------------
+          case DELETE_TASK:
+      
+    const deletedTaskIndex = state.taskItems.findIndex((task) => task.text === action.payload);
+      if (deletedTaskIndex !== -1) {
+
+        const deletedTask = state.taskItems[deletedTaskIndex];
+        const updatedTaskItems = state.taskItems.slice(0, deletedTaskIndex).concat(state.taskItems.slice(deletedTaskIndex + 1));
+          console.log("Deleted Task:", deletedTask);
+
+        return {
+          ...state,
+          taskItems: updatedTaskItems,
+          deletedTasks: state.deletedTasks.concat(deletedTask),
+        };
       }
+      return state;
+
+ 
     default:
       return state;
   }
